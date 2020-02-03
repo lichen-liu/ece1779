cd17c75bea9381dad0be9c87cdd1eb5c31246e8d
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-02-02 21:56:27 -0500

    Add very basic accounts login and register feature

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 webapp = Flask(__name__)
 
-from app import trivial
-from app import courses
-from app import students
-from app import sections
+# from app import trivial
+# from app import courses
+# from app import students
+# from app import sections
+
+from app import account
 
 from app import main
 
---
+++ app/account.py
@@ -0,0 +1,31 @@
+from flask import render_template, redirect, url_for, request, g
+from app import webapp
+
+
+@webapp.route('/api/account_actions', methods=['POST'])
+# Web handler for account actions
+def account_actions_handler():
+    account_action_type = request.form.get('action')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if account_action_type == 'login':
+        return account_login(username, password)
+    elif account_action_type == 'register':
+        return account_register(username, password)
+    else:
+        # No other actions are supported yet
+        assert 0
+
+@webapp.route('/api/register', methods=['POST'])
+# API handler for account register
+def account_register_handler():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return account_register(username, password)
+
+def account_register(username, password):
+    print('Registering: u=' + username + ' p=' + password)
+    return 'Registering: u=' + username + ' p=' + password
+
+def account_login(username, password):
+    return 'Login: u=' + username + ' p=' + password
