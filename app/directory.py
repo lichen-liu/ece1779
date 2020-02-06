c51cf5f3d480ef16577c1392f174b323f8b81707
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-02-05 23:48:36 -0500

    disable thumbnail + refactor directory

--- app/directory.py
+++ app/directory.py
@@ -2,60 +2,67 @@ import os
 from app import account
 
 
-def get_relative_user_root_path_in_static():
-    return os.path.join('users_data/', account.account_get_logged_in_user())
+def get_static_dir_path():
+    return os.path.join(os.getcwd(), 'static')
 
 
-def get_relative_user_photo_path_in_static():
-    return os.path.join(get_relative_user_root_path_in_static(), 'photos')
+def get_root_dir_path(absolute=True):
+    root_dir_name = 'users_data'
+    if absolute:
+        return os.path.join(get_static_dir_path(), root_dir_name)
+    else:
+        return root_dir_name
 
 
-def get_relative_user_thumbnail_path_in_static():
-    return os.path.join(get_relative_user_root_path_in_static(), 'thumbnails')
+def get_user_dir_path(absolute=True):
+    if absolute:
+        return os.path.join(get_root_dir_path(absolute), account.account_get_logged_in_user())
+    else:
+        return get_root_dir_path(absolute) + '/' + account.account_get_logged_in_user()
 
 
-def get_absolute_path_to_static_folder():
-    return os.path.join(os.getcwd(), 'app/static')
+def get_user_photos_dir_path(absolute=True):
+    if absolute:
+        return os.path.join(get_user_dir_path(absolute), 'photos')
+    else:
+        return get_user_dir_path(absolute) + '/' + 'photos'
 
 
-def get_root_data_directory_path():
-    return os.path.join(get_absolute_path_to_static_folder(), 'users_data')
+def get_user_thumbnails_dir_path(absolute=True):
+    if absolute:
+        return os.path.join(get_user_dir_path(absolute), 'thumbnails')
+    else:
+        return get_user_dir_path(absolute) + '/' + 'thumbnails'
 
 
-def get_user_data_directory_path():
-    return os.path.join(get_absolute_path_to_static_folder(), get_relative_user_root_path_in_static())
+def create_static_dir_if_necessary():
+    if not os.path.exists(get_static_dir_path()):
+        os.mkdir(get_static_dir_path())
 
 
-def get_user_photo_directory_path():
-    return os.path.join(get_absolute_path_to_static_folder(), get_relative_user_photo_path_in_static())
+def create_root_dir_if_necessary():
+    if not os.path.exists(get_root_dir_path()):
+        os.mkdir(get_root_dir_path())
 
 
-def get_user_thumbnail_directory_path():
-    return os.path.join(get_absolute_path_to_static_folder(), get_relative_user_thumbnail_path_in_static())
+def create_user_dir_if_necessary():
+    if not os.path.exists(get_user_dir_path()):
+        os.mkdir(get_user_dir_path())
 
 
-def create_root_directory():
-    if not os.path.exists(get_root_data_directory_path()):
-        os.mkdir(get_root_data_directory_path())
+def create_user_photos_dir_if_necessary():
+    if not os.path.exists(get_user_photos_dir_path()):
+        os.mkdir(get_user_photos_dir_path())
 
 
-def create_user_data_directory():
-    if not os.path.exists(get_user_data_directory_path()):
-        os.mkdir(get_user_data_directory_path())
-
-
-def create_user_photo_directory():
-    if not os.path.exists(get_user_photo_directory_path()):
-        os.mkdir(get_user_photo_directory_path())
-
-
-def create_user_thumbnail_directory():
-    if not os.path.exists(get_user_thumbnail_directory_path()):
-        os.mkdir(get_user_thumbnail_directory_path())
+def create_user_thumbnails_dir_if_necessary():
+    if not os.path.exists(get_user_thumbnails_dir_path()):
+        os.mkdir(get_user_thumbnails_dir_path())
 
 
 def create_directory_if_necessary():
-    create_root_directory()
-    create_user_data_directory()
-    create_user_photo_directory()
-    create_user_thumbnail_directory()
+    create_static_dir_if_necessary()
+    create_root_dir_if_necessary()
+    create_user_dir_if_necessary()
+    create_user_photos_dir_if_necessary()
+    create_user_thumbnails_dir_if_necessary()
--- app/photo.py
+++ app/photo.py
@@ -1,9 +1,10 @@
 
 import os
 from flask import (redirect, render_template, request, url_for, current_app)
-#The handour says you CAN user MaxxMagic, I think it means we can use anything we want 
-from PIL import Image
-from app import webapp, path, account
+#The handour says you CAN user MaxxMagic, I think it means we can use anything we want
+# they will run loadtester, we better stick with the handout to avoid risks 
+#from PIL import Image
+from app import webapp, directory, account
 from urllib.parse import unquote_plus
 
 
@@ -12,36 +13,39 @@ def photo_upload_handler():
    
     photo_file = request.files['file']
     filename = photo_file.filename
-    photo_file.save(os.path.join(path.get_user_photo_directory_path(), filename))
+    photo_file.save(os.path.join(directory.get_user_photos_dir_path(), filename))
 
-    thumbnail = get_thumbnail_for_image(photo_file)
-    thumbnail.save(os.path.join(path.get_user_thumbnail_directory_path(), filename))
+    # thumbnail = get_thumbnail_for_image(photo_file)
+    # thumbnail.save(os.path.join(directory.get_user_thumbnail_directory_path(), filename))
    
     return redirect('/')
     #return render_template('user_welcome.html',title='Photo Upload Successfully', username=account.account_get_logged_in_user())
 
-#Just use this for now, my friend
-def get_thumbnail_for_image(image_file):
-    image = Image.open(image_file)
-    image.thumbnail(current_app.config["THUMBNAIL_SIZE"], Image.ANTIALIAS)
-    return image
+
+# #Just use this for now, my friend
+# # it crashes on my computer
+# def get_thumbnail_for_image(image_file):
+#     image = Image.open(image_file)
+#     image.thumbnail(current_app.config["THUMBNAIL_SIZE"], Image.ANTIALIAS)
+#     return image
 
 
 @webapp.route('/api/thumbnail_display', methods=['POST'])
 def display_thumbnails():
 
-    all_user_thumbnails = os.listdir(path.get_user_thumbnail_directory_path())
+    all_user_thumbnails = os.listdir(directory.get_user_photos_dir_path())
 
     return render_template('thumbnail_display.html', \
     thumbnails = all_user_thumbnails, \
-    root_path = path.get_relative_user_thumbnail_path_in_static())
+    root_path = directory.get_user_photos_dir_path(False))
+
 
 @webapp.route('/api/photo_display', methods=['GET'])
 def display_photo():
 
     return render_template('display_photo.html', \
     photo_name = unquote_plus(request.args.get('file_name')), \
-    root_path = path.get_relative_user_photo_path_in_static())
+    root_path = directory.get_user_photos_dir_path(False))
 
 
 # Mock database for photos
