d4a15c2dc5045135bf6b50b6b923294e98865a37
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-02-06 22:08:33 -0500

    Commented out run.py, as it is not recommended

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
-#!venv/bin/python
-from app import webapp, directory
+# #!venv/bin/python
+# from app import webapp, directory
 
-def run_app():
-    prepare()
-    run_flask()
+# def run_app():
+#     init()
+#     run_flask()
 
-def prepare():
-    directory.create_static_dir_if_necessary()
-    directory.create_root_dir_if_necessary()
+# def init():
+#     directory.create_static_dir_if_necessary()
+#     directory.create_root_dir_if_necessary()
 
-def run_flask():
-    webapp.run(host='0.0.0.0',debug=True)
+# def run_flask():
+#     webapp.run(host='0.0.0.0',debug=True)
 
-run_app()
+# run_app()
