8f5bae842aff1b057ed75d125277a14a3e5f8f9b
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-02-12 23:27:50 -0500

    fix config in __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 webapp = Flask(__name__)
 
 from app import config
-webapp.config.from_object('config.Config')
+webapp.config.from_object('app.config.Config')
 
 from app import initialize
 initialize.init()
