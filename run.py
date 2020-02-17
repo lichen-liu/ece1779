326b9140e89719394b16d108c39a7028f8211022
bwuzhang <ubuntu@ip-172-31-38-146.ec2.internal>
2020-02-17 18:03:16 +0000

    Update run.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!venv/bin/python
 import app
-
+from app import webapp
 if __name__ == '__main__':
 	app.init()
 	#app.webapp.run(host='127.0.0.1',debug=True)
-	app.webapp.run(host='127.0.0.1')
+	webapp.run(host='127.0.0.1')
--- run_server.py
+++ run_server.py
@@ -1,6 +1,6 @@
 #!venv/bin/python
 import app
-
+from app import webapp
 if __name__ == '__main__':
 	app.init()
-	app.webapp.run(host='0.0.0.0')
+	webapp.run(host='0.0.0.0')
