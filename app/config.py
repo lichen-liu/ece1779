d2f1d48ba91a6eda7beba7753005dca47b3cb329
rayHrx <huang627158768@gmail.com>
2020-02-17 20:30:23 -0500

    change alive session time to 24hrs

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ from datetime import timedelta
 
 class Config(object):
     SECRET_KEY = 'NIDEMAMAMASHANGJIUYAOBAOZHALE'
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=100)
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes= 60 * 24)
     SESSION_REFRESH_EACH_REQUEST = True
     MAXIMUM_IMAGE_SIZE = 20 * 1024 * 1024
     ALLOWED_IMAGE_EXTENSION = set(['.png', '.jpg', '.jpeg', '.gif']) # Lower case
-    IMAGE_PROCESSING_CHOICE = 0
+    IMAGE_PROCESSING_CHOICE = 1
