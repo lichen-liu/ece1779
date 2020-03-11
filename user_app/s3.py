ecbc36fdc7e5ed0f0bdf531197736133f4f4bdb0
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-10 23:21:44 -0400

    Fixed directory-related query functions

--- user_app/s3.py
+++ user_app/s3.py
@@ -33,21 +33,85 @@ def create_bucket_if_necessary(bucket_name):
         l_s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
 
 
-def list_bucket_content(bucket_name):
+def list_bucket_content(bucket_name, directory='', recursive=True):
+    '''List the content in an S3 bucket directory (optional)
+
+    :param bucket_name: Bucket to check
+    :param directory: Directory to check. Optional. Does not support partial name
+    :param recursive: True to list the content recursively, else only list the content inside dir
+    :return: [(object, size)]
+    '''
+    if len(directory) > 0:
+        assert directory[-1] == '/'
     paginator = l_s3.get_paginator('list_objects_v2')
-    pages = paginator.paginate(Bucket=bucket_name)
+    pages = paginator.paginate(Bucket=bucket_name, Prefix=directory)
 
     result = list()
     for item in pages.search('Contents'):
         if item is not None:
-           result.append(item['Key'])
+            key = item['Key']
+            subkey = key[len(directory):]
+
+            if len(subkey) == 0:
+                continue
+
+            if not recursive:
+                slash_count = subkey.count('/')
+                if slash_count > 1 or (slash_count == 1 and subkey[-1] != '/'):
+                    continue
+
+            result.append((key, item['Size']))
 
     return result
 
 
+def get_bucket_content_size(bucket_name, key):
+    '''Get the size of an S3 bucket object
+
+    :param bucket_name: Bucket to check
+    :param key: Object to check. Does not support partial name
+    :return: (size, num_directory, num_file, [object])
+    '''
+    paginator = l_s3.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=bucket_name, Prefix=key)
+
+    size = 0
+    l = list()
+    dir_count = 0
+    file_count = 0
+    for item in pages.search('Contents'):
+        if item is not None:
+            k = item['Key']
+            subkey = k[len(key):]
+
+            if len(subkey) == 0 or len(key) == 0 or (len(key) > 0 and key[-1] == '/'):
+                size += item['Size']
+
+                if k[-1] == '/':
+                    dir_count += 1
+                else:
+                    file_count += 1
+
+                l.append(k)
+    return (size, dir_count, file_count, l)
+
+
 def is_object_existed(bucket_name, key):
-    response = l_s3.list_objects(Bucket=bucket_name, Prefix=key)
-    return 'Contents' in response
+    '''Check whether an object exists in an S3 bucket object
+
+    :param bucket_name: Bucket to check
+    :param key: Object to check. Does not support partial name
+    :return: True if found, else False
+    '''
+    paginator = l_s3.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=bucket_name, Prefix=key)
+
+    for item in pages.search('Contents'):
+        if item is not None:
+            if item['Key'] == key:
+                return True
+
+    return False
 
 
 def create_directory_if_necessary(bucket_name, dir_name):
@@ -61,6 +125,9 @@ def create_directories_if_necessary(bucket_name):
     create_directory_if_necessary(bucket_name, PHOTOS_DIR)
     create_directory_if_necessary(bucket_name, THUMBNAILS_DIR)
     create_directory_if_necessary(bucket_name, RECTANGLES_DIR)
+
+    create_directory_if_necessary(bucket_name, 'asd')
+    create_directory_if_necessary(bucket_name, 'asd//a')
 
 
 def upload_object(bucket_name, key, file):
@@ -94,6 +161,9 @@ def get_object_url(bucket_name, key):
 
 
 def delete_directory_content(bucket_name, directory):
+    if len(directory) > 0:
+        assert directory[-1] == '/'
+
     paginator = l_s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix=directory)
 
@@ -118,8 +188,26 @@ def delete_object(bucket_name, key):
     l_s3.delete_object(Bucket=bucket_name, Key=key)
 
 
-'''
-TODO: print number of non-dir objects
-TODO: print size of dir
-TODO: list_bucket_content filter out file only option
-'''
+
+# init()
+# key = 'data/photos/DSCF0034.JP'
+# print('key',key)
+# try:
+#     print('list_bucket_content', list_bucket_content(BUCKET,key, True))
+# except Exception as e:
+#     print('list_bucket_content')
+
+# try:
+#     print('list_bucket_content', list_bucket_content(BUCKET,key, False))
+# except Exception as e:
+#     print('list_bucket_content')
+
+# try:
+#     print('get_bucket_content_size', get_bucket_content_size(BUCKET, key))
+# except Exception as e:
+#     print('get_bucket_content_size')
+
+# try:
+#     print('is_object_existed', is_object_existed(BUCKET, key))
+# except Exception as e:
+#     print('is_object_existed')
