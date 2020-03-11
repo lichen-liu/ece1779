8ea08e8b07b6e76636c00f535596eb9c786484ca
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-10 23:33:09 -0400

    hide all s3 client in s3

--- user_app/s3.py
+++ user_app/s3.py
@@ -8,7 +8,7 @@ import botocore
 - file name can be the same as directory. But directory must end with '/', whereas file must not
 '''
 
-l_s3 = None
+_s3 = None
 
 BUCKET = 'ece1779'
 ROOT_DIR = 'data' + '/'
@@ -18,8 +18,8 @@ RECTANGLES_DIR = ROOT_DIR + 'rectangles' + '/'
 
 
 def init():
-    global l_s3
-    l_s3 = boto3.client('s3')
+    global _s3
+    _s3 = boto3.client('s3')
 
     # Remove the following
     create_bucket_if_necessary(BUCKET)
@@ -27,7 +27,7 @@ def init():
 
 
 def is_bucket_existed(bucket_name):
-    response = l_s3.list_buckets()
+    response = _s3.list_buckets()
 
     if 'Buckets' in response:
         return bucket_name in [bucket['Name'] for bucket in response['Buckets']]
@@ -37,7 +37,7 @@ def is_bucket_existed(bucket_name):
 def create_bucket_if_necessary(bucket_name):
     if not is_bucket_existed(bucket_name):
         print('Creating bucket', bucket_name, '!')
-        l_s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
+        _s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
 
 
 def list_bucket_content(bucket_name, directory='', recursive=True):
@@ -50,7 +50,7 @@ def list_bucket_content(bucket_name, directory='', recursive=True):
     '''
     if len(directory) > 0:
         assert directory[-1] == '/'
-    paginator = l_s3.get_paginator('list_objects_v2')
+    paginator = _s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix=directory)
 
     result = list()
@@ -79,7 +79,7 @@ def get_bucket_content_size(bucket_name, key):
     :param key: Object to check. Does not support partial name
     :return: (size, num_directory, num_file, [object])
     '''
-    paginator = l_s3.get_paginator('list_objects_v2')
+    paginator = _s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix=key)
 
     size = 0
@@ -110,7 +110,7 @@ def is_object_existed(bucket_name, key):
     :param key: Object to check. Does not support partial name
     :return: True if found, else False
     '''
-    paginator = l_s3.get_paginator('list_objects_v2')
+    paginator = _s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix=key)
 
     for item in pages.search('Contents'):
@@ -124,7 +124,7 @@ def is_object_existed(bucket_name, key):
 def create_directory_if_necessary(bucket_name, dir_name):
     if not is_object_existed(bucket_name, dir_name):
         print('Creating directory', dir_name, 'in bucket', bucket_name, '!')
-        l_s3.put_object(Bucket=bucket_name, Key=dir_name)
+        _s3.put_object(Bucket=bucket_name, Key=dir_name)
 
 
 def create_directories_if_necessary(bucket_name):
@@ -144,7 +144,7 @@ def upload_object(bucket_name, key, file):
     '''
 
     try:
-        l_s3.upload_fileobj(file, bucket_name, key)
+        _s3.upload_fileobj(file, bucket_name, key)
     except botocore.exceptions.ClientError as e:
         print(str(e))
         return False
@@ -161,14 +161,14 @@ def get_object_url(bucket_name, key):
 
     if not is_object_existed(bucket_name, key):
         return None
-    return l_s3.generate_presigned_url('get_object', ExpiresIn=3600, Params={'Bucket': bucket_name, 'Key': key})
+    return _s3.generate_presigned_url('get_object', ExpiresIn=3600, Params={'Bucket': bucket_name, 'Key': key})
 
 
 def delete_directory_content(bucket_name, directory):
     if len(directory) > 0:
         assert directory[-1] == '/'
 
-    paginator = l_s3.get_paginator('list_objects_v2')
+    paginator = _s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix=directory)
 
     delete_us = dict(Objects=[])
@@ -180,16 +180,16 @@ def delete_directory_content(bucket_name, directory):
 
         # flush once aws limit reached
         if len(delete_us['Objects']) >= 1000:
-            l_s3.delete_objects(Bucket=bucket_name, Delete=delete_us)
+            _s3.delete_objects(Bucket=bucket_name, Delete=delete_us)
             delete_us = dict(Objects=[])
 
     # flush rest
     if len(delete_us['Objects']):
-        l_s3.delete_objects(Bucket=bucket_name, Delete=delete_us)
+        _s3.delete_objects(Bucket=bucket_name, Delete=delete_us)
 
 
 def delete_object(bucket_name, key):
-    l_s3.delete_object(Bucket=bucket_name, Key=key)
+    _s3.delete_object(Bucket=bucket_name, Key=key)
 
 
 
