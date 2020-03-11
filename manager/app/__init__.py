9f069cc4238dfe0d35ba0d8fa95b9721a2d7d694
rayHrx <huang627158768@gmail.com>
2020-03-10 20:50:36 -0400

    Remove old manager folder

--- manager/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-webapp = Flask(__name__)
-
-from app import manager_main, worker_count_monitor
-
-monitor = worker_count_monitor.get_worker_count_monitor()
-monitor.start()
--- manager/run.py
+++
@@ -1,4 +0,0 @@
-import app
-from app import webapp
-if __name__ == '__main__':
-    webapp.run(host='127.0.0.1')
