8ac7519f18d63edbcddc3a1efbc95085b2ea1716
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-12 01:45:33 -0400

    update start_user_app.sh

--- start_user_app.sh
+++ start_user_app.sh
@@ -1,7 +1,9 @@
 # This only works on AWS EC2 instance used for deployment."
 
 if ["$1" = "git"]; then
-    echo "git pull"
+    echo "> git checkout -f"
+    git checkout -f
+    echo "> git pull"
     git pull
 fi
 python3 -m venv venv
