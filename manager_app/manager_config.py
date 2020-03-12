0f04fdb58dd9f4ba9e5b0ed1ed4ec2ecee73e0a0
bwuzhang <ubuntu@ip-172-31-38-146.ec2.internal>
2020-03-12 22:55:21 +0000

    add manager config file

---
+++ manager_app/manager_config.py
@@ -0,0 +1,12 @@
+HOSTING_EC2_INSTANCE_ID = 'i-01cd98e05b6a9457f'
+DEFAULT_LOAD_BALANCER_INDEX = 0
+DEFAULT_TARGET_GROUP_INDEX = 0
+MIN_WORKER_NUM = 1
+def get_hosting_ec2_id():
+    return HOSTING_EC2_INSTANCE_ID
+def get_default_load_balancer_index():
+    return DEFAULT_LOAD_BALANCER_INDEX
+def get_default_target_group_index():
+    return DEFAULT_TARGET_GROUP_INDEX
+def get_min_worker_num():
+    return MIN_WORKER_NUM
