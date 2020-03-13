f9a3b085482acedcb511c62ae3eb7ba3908f13e2
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-13 18:26:00 -0400

    removed start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-# This only works on AWS EC2 instance used for deployment."
-
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate base 
-cd /home/ubuntu/ece1779
-gunicorn --bind 0.0.0.0:5000 --workers=1 run_user_app:webapp
