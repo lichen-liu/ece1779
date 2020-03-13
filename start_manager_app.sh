992a4f0ea1c64c27f5a81299f958a0f29eee3baf
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-12 21:10:32 -0400

    improve the messages on both sh

--- start_manager_app.sh
+++ start_manager_app.sh
@@ -1,21 +1,22 @@
+#!/bin/bash
+
+# This only works on AWS EC2 instance used for deployment.
+
 if [ "$1" = "git" ]; then
-echo "> Update repository"
-git checkout -f
-git pull
-chmod 777 "${0}"
+    echo "> Update repository"
+    git checkout -f
+    git pull
+    chmod 777 "${0}"
 fi
-echo "> Initialize virtual environment... -f"
+echo "> Initialize virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
-echo "> Installing Flask..."
+echo "> Installing Libraries..."
 pip install Flask
-echo "> Installing MySql connector..."
 pip install mysql-connector-python
-echo "> Installing AWS command line interface..."
 pip install awscli
-echo "> Installing Boto3..."
 pip install boto3
-echo "> Installing Gunicorn..."
 pip install gunicorn
+
 echo "> Starting the manager app on Port = 5000 ..."
 venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 run_manager_app:webapp
--- start_user_app.sh
+++ start_user_app.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# This only works on AWS EC2 instance used for deployment."
+# This only works on AWS EC2 instance used for deployment.
 
 if [ "$1" = "git" ]; then
-    echo "> git checkout -f"
+    echo "> Update repository"
     git checkout -f
-    echo "> git pull"
     git pull
-    echo "> chmod 777 ${0}"
     chmod 777 "${0}"
 fi
+echo "> Initialize virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
+echo "> Installing Libraries..."
 pip install Flask
 pip install mysql-connector-python
 pip install opencv-python
@@ -19,4 +19,6 @@ pip install cvlib
 pip install tensorflow
 pip install boto3
 pip install gunicorn
+
+echo "> Starting the user app on Port = 5000 ..."
 venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 run_user_app:webapp
