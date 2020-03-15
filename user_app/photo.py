3b4a01dc8b5790f5fe515a606b9ec4bee0951ab1
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-15 04:02:27 -0400

    api improvement for aws op

--- user_app/photo.py
+++ user_app/photo.py
@@ -2,7 +2,7 @@ import os
 
 from flask import redirect, render_template, request, current_app, abort, jsonify
 from user_app import webapp, account, main, image_processing
-from common_lib import utility, database, s3
+from common_lib import utility, database, s3, combined_aws
 
 
 @webapp.route('/api/upload', methods=['POST'])
@@ -126,17 +126,8 @@ def delete_photo(photo_id):
     if userid != account.account_get_logged_in_userid():
         return 'Operation is not allowed!'
 
-    print('Deleting ', photo_id, photo_name)
-    saved_photo_file_name = str(photo_id) + utility.get_file_extension(photo_name)
-    photo_s3_key = s3.PHOTOS_DIR + saved_photo_file_name
-    thumbnail_s3_key = s3.THUMBNAILS_DIR + saved_photo_file_name
-    rectangle_s3_key = s3.RECTANGLES_DIR + saved_photo_file_name
-
-    s3.delete_object(key=photo_s3_key)
-    s3.delete_object(key=thumbnail_s3_key)
-    s3.delete_object(key=rectangle_s3_key)
-
-    database.delete_photo(photo_id)
+    print('Deleting', photo_id, photo_name)
+    combined_aws.delete_photo_from_s3_and_database(photo_id, photo_name)
 
 
 def is_extension_allowed(filename):
