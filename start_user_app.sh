ca62cefbe8e7e67422b1677d0e65a2e89d64f515
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-14 21:54:27 -0400

    update start_user_app.sh, redirect dummy output

--- start_user_app.sh
+++ start_user_app.sh
@@ -21,7 +21,7 @@ pip install boto3
 pip install gunicorn
 
 echo "> Starting the dummy app on Port = 5001 ..."
-venv/bin/gunicorn --bind 0.0.0.0:5001 --workers=1 run_dummy_app:webapp | tee dummy_app.log &
+venv/bin/gunicorn --bind 0.0.0.0:5001 --workers=1 run_dummy_app:webapp &> dummy_app.log &
 
 echo "> Starting the user app on Port = 5000 ..."
 venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 run_user_app:webapp
