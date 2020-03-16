ca317519947cb0d3d964f3973c474c55502724fa
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-03-16 05:57:40 -0400

    fix dependency on manager_app script

--- start_manager_app.sh
+++ start_manager_app.sh
@@ -17,6 +17,8 @@ pip install mysql-connector-python
 pip install awscli
 pip install boto3
 pip install gunicorn
+# For python 3.5, this package is available on python 3.6 by default
+pip install python2-secrets
 
 echo "> Starting the manager app on Port = 5000 ..."
 venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 run_manager_app:webapp
